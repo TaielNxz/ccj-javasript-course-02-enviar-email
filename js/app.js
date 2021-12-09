cb7327e44e2a348316b8bb03105a8b70927a25c1
TaielNxz <TaielNxz@hotmail.com>
2021-12-09 15:48:59 -0300

    validar email con una expresión regular

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@ const email   = document.querySelector('#email');
 const asunto  = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+// Expresión regular
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
 /* =========================================
               EventListeners
@@ -44,8 +47,22 @@ function validarFormulario(e) {
         e.target.classList.remove('border-green-500');
     }
 
+    // Si el campo es el de Email
+    if( e.target.type === 'email') {
+        // validar con expresión regular
+        if ( er.test( e.target.value ) ) {
+            // Agregar borde rojo
+            e.target.classList.remove('border-red-500');
+            e.target.classList.add('border-green-500');
+        } else {
+            // Agregar borde verde
+            e.target.classList.remove('border-green-500');
+            e.target.classList.add('border-red-500');
+        }
+    }
+
     // Si todos los campos son validos
-    if( email.value && asunto.value !== '' && mensaje.value !== '' ) {
+    if( er.test(email.value) && asunto.value !== '' && mensaje.value !== '' ) {
         // Habilitar el boton "Enviar"
         btnEnviar.disabled = false;
         btnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
